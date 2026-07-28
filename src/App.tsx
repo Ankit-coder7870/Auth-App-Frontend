52c9c39f3d1add6e43bc2ebf5176b0cf0dfc63f2
ankit-coder7870 <ankitanand1774@gmail.com>
2026-07-28 09:58:53 +0530

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import {Button} from "./components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter,Routes,Route} from "react-router";
@@ -7,15 +6,18 @@ import Login from './pages/Login.tsx';
 import Signup from './pages/Signup.tsx';
 import Service from './pages/Service.tsx';
 import About from './pages/About.tsx';
+import RootLayout from './pages/RootLayout.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
   <Routes>
-    <Route path='/' element = {<App/>}/>
-    <Route path='/login' element = {<Login/>}/>  
-    <Route path='/signup' element = {<Signup/>}/>
-    <Route path='/service' element = {<Service/>}/>
-    <Route path='/about' element = {<About/>}/>  
+    <Route path='/' element = {<RootLayout/>}>
+      <Route index element = {<App/>}/>
+      <Route path='/login' element = {<Login/>}/>  
+       <Route path='/signup' element = {<Signup/>}/>
+      <Route path='/service' element = {<Service/>}/>
+      <Route path='/about' element = {<About/>}/>
+      </Route>  
   </Routes>
   </BrowserRouter>
   ,
